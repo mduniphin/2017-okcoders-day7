8d6dc7bc2b15e87edf9eb84abbe8bbb5f8a1b14d
mduniphin <maxx.duniphin@gmail.com>
2017-04-29 12:05:24 -0500

    bcrypt

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,20 +1,36 @@
 var Auth = require('../../models/auth') //getting model
-//creating model
-exports.create = function(req, res, next){
+var bcrypt = require('bcrypt');
+const saltRounds = 2;
+
+exports.create = function(req, res, next){ //creating model
 	var auth = new Auth();
-	auth.username = req.body.username;
-	auth.password = req.body.password;
+	var user = req.body.username;
+	var pass = req.body.password;
 
-	auth.date = new Date();
-	auth.is_active = true;
-
-	auth.save(function(err, data){
-		if(err){
-			console.log("Error saving to db: " + err);
-			res.send(404, "Error with database");
-		} else {
-			res.json(201, {status: "success"}); //if saved
-		}
+	if(!user || !pass){ //check for username and password
+		res.send(404, "Username or password missing");
 		return next();
+	}
+
+	bcrypt.hash(pass, saltRounds, function(err, hash){
+		if(err){
+			console.log(err);
+			res.send(404);
+		} 	else {
+		  	auth.username = user;
+		  	auth.password = hash;
+		  	auth.date = new Date();
+	      	auth.is_active = true;
+
+	      	auth.save(function(err, data){
+				if(err){
+					console.log("Error saving to db: " + err);
+					res.send(404, "Error with database");
+				} 	else {
+				  	res.json(201, {status: "success"}); //if saved
+				}
+			});
+			return next();
+		}			
 	});
 }
